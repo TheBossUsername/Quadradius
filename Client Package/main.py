32bae4d5b383251672dfbc453f55af4941fb785e
TheBossUsername <scot226@live.com>
2023-10-29 12:23:55 -0600

    bug

--- Client Package/main.py
+++ Client Package/main.py
@@ -3,7 +3,7 @@ from classes.game import Game
 server_ip = '10.50.169.241' # Put the IP address of the computer hosting the server
 server_port = 12345 # Put the port the server is running on
 
-def main():
+def main(server_ip, server_port):
     game = Game(server_ip, server_port)
     game.run_game()
 
